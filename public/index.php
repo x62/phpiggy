fa63aa6e58388412e5bf3e05ae8ce5f3be768cbb
Paul Brugger <paul@brugger.com>
2024-01-23 10:02:32 -0700

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+// this is index.php
+
+$app = include __DIR__ . '/../src/App/bootstrap.php';
+
+$app->run();
+
---
+++ src/App/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+// include __DIR__ . "/../Framework/App.php";
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
